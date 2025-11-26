d03907c237370c5b740f5abbb2ef7e9422c4c432
Sifatun <sifanoor04@gmail.com>
2025-11-25 22:59:49 -0500

    Added new events and functions to interface

--- interfaces/LostPetInterface.sol
+++ interfaces/LostPetInterface.sol
@@ -19,7 +19,9 @@ interface ILostPet {
 
     // Events defining the signals the contract emits
     event CaseCreated(uint256 caseId, address owner, string petName);
+    event FinderSubmitted(uint256 indexed caseId, address indexed finder);
     event CaseResolved(uint256 caseId, address finder);
+    event CaseCancelled(uint256 indexed caseId, address indexed owner);
     
     // --- Function Headers ---
 
@@ -42,6 +44,16 @@ interface ILostPet {
      * @notice Get finders for a case
      */
     function getFinders(uint256 caseId) external view returns (address[] memory);
+
+    /**
+     * @notice Check if an address is a finder for a specific case
+     */
+    function isFinder(uint256 caseId, address finder) external view returns (bool isFinder);
+
+    /**
+     * @notice Get detailed information about a specific case
+     */
+    function getCaseInfo(uint256 caseId) external view returns (address owner, string memory petName, uint256 bounty, bool isResolved, uint256 finderCount);
     
     /**
      * @notice Cancel case and get refund
